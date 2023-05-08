c0e123e2196412be20370817e3cea2de1eef3dfb
Yanny Nuñez <yannyesteban@hotmail.com>
2023-05-07 20:54:07 -0400

    homE

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,27 @@
-var http = require('http');
+import { Lexer } from "./core/Lexer.js";
+import { AST } from "./core/AST.js";
+import * as fs from "fs";
+import * as http from "http";
 //create a server object:
 http.createServer(function (req, res) {
     console.log("prueba");
     res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
+    fs.readFile("sevian.sv", function (err, buff) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log();
+        var source = buff.toString();
+        var lexer = new Lexer(source);
+        var tokens = lexer.getTokens();
+        console.log(source, "\n", tokens);
+        var ast = new AST(tokens);
+        var statements = ast.parse();
+        console.log("result", statements);
+        console.log("bye", JSON.stringify(statements));
+        res.write(JSON.stringify(statements));
+        res.end(); //end the response
+    });
 }).listen(8080); //the server object listens on port 8080 
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,8 +1,39 @@
-var http = require('http');
+import { Lexer } from "./core/Lexer.js";
+import { AST } from "./core/AST.js";
+
+import * as fs from "fs";
+
+import * as http from "http"
 
 //create a server object:
 http.createServer(function (req, res) {
   console.log("prueba")
   res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
+
+
+  fs.readFile("sevian.sv", (err, buff) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log();
+
+    const source = buff.toString();
+    const lexer = new Lexer(source);
+
+    const tokens = lexer.getTokens();
+
+    console.log(source, "\n", tokens);
+
+    const ast = new AST(tokens);
+
+    const statements = ast.parse();
+    console.log("result", statements);
+    console.log("bye", JSON.stringify(statements));
+    res.write(JSON.stringify(statements));
+
+    res.end(); //end the response
+  });
+
 }).listen(8080); //the server object listens on port 8080 
